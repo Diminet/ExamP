a14d7fba90180bcc4d4d4cd1f9f8b928a322cbc4
Diminet <70798333+Diminet@users.noreply.github.com>
2021-06-08 00:59:16 +0300

    Add files via upload

---
+++ test/core/AbstractPOM.java
@@ -0,0 +1,12 @@
+package org.example.test.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class AbstractPOM {
+    protected WebDriver driver ;
+    public AbstractPOM(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this );
+    }
+}
